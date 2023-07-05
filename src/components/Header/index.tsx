87f2d2f446e00cc269e3c8d57999d35b17f4fe82
Luciano Fernandes <lucianoffernands@gmail.com>
2023-07-04 21:19:24 -0300

    Component Header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,12 @@
+import './styles.css';
+
+export default function Header() {
+
+    return (
+        <header>
+            <div className="dsct-container">
+                <h1>DSCarrosTop</h1>
+            </div>
+        </header>
+    );
+}
---
+++ src/routes/Catalog/index.tsx
@@ -0,0 +1,11 @@
+import './styles.css';
+import Header from "../../components/Header";
+
+export default function Catalog() {
+
+    return (
+        <>
+           <Header /> 
+        </>
+    );
+}
