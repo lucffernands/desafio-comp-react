c17d782fa4b62b2b4d435bc4751e73f015918f8f
Luciano Fernandes <lucianoffernands@gmail.com>
2023-07-06 19:15:22 -0300

    Component CardCar

--- src/routes/Catalog/index.tsx
+++ src/routes/Catalog/index.tsx
@@ -1,11 +1,23 @@
 import './styles.css';
 import Header from "../../components/Header";
+import CardCar from '../../components/CardCar';
 
 export default function Catalog() {
 
     return (
         <>
-           <Header /> 
+            <Header />
+            <main className="dsct-main-container">
+                <section id="dsct-card-car">
+                    <div className="dsct-card-title">
+                        <h3>Venha nos visitar</h3>
+                    </div>
+                    <div className="dsct-card-car-container">
+                        <CardCar />
+                        <CardCar />
+                    </div>
+                </section>
+            </main>
         </>
     );
 }
